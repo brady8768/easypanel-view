383061406717eb3a5c3336ffb5dfea0821fc9773
cangyunml <vikghost@163.com>
2021-05-19 14:44:37 +0800

    fix

--- public/install/templates/step1.php
+++ public/install/templates/step1.php
@@ -13,8 +13,9 @@
       <pre class="pact" readonly="readonly">
           <h1 style="text-align: center;">软件介绍</h1>
 
-<strong>UFO视频加速器面板（以下称“UFO”），由UFO小组独创开发，基于 PHP + MySQL 的技术，采用ThinkPHP6框架开发。</strong>
-UFO官方网站：http://www.ufo.com
+
+<strong> EasyPanel 是由 EasyPanel 团队 开发的一系列与计算机网络相关的软件。集成了用户前端、管理后端、支付、防护以及资源推广的一系列的服务。EasyPanel不仅仅只是个面板，它为运营者提供了一整套的解决方案。</strong>
+<br><br><br>EasyPanel网址：https://easypanel.xyz
 
 </pre>
     </div>
