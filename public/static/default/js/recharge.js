ab3b0389aac9751b3428bede2a6341a5a57a7663
cangyunml <vikghost@163.com>
2021-05-20 16:42:14 +0800

    fix

--- public/static/default/js/recharge.js
+++ public/static/default/js/recharge.js
@@ -37,7 +37,7 @@ $(function () {
                     height: 256,
                     background: "#ffffff",
                     foreground: "#000000",
-                    src: "/static/img/log.png"
+                    src: "../img/log.png"
                 });
                 $('#gridSystemModal').modal('show');
             }else{
